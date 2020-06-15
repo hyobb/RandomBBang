d24a65598007164713f61dc46e66fc956803f0be
Ryu Hyo Kwang <hkryu0719@gmail.com>
2020-06-15 11:25:48 +0900

    Initial commit

---
+++ RandomBBang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomBBang
+//
+//  Created by 류효광 on 2020/06/15.
+//  Copyright © 2020 StudioX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
