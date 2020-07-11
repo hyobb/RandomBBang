5be65a9857b992d079ab8427b4373def4f8c6202
Ryu Hyo Kwang <hkryu0719@gmail.com>
2020-07-11 13:24:04 +0900

    feat: add reactor code for player count

--- RandomBBang/HomeViewReactor.swift
+++ RandomBBang/HomeViewReactor.swift
@@ -12,17 +12,46 @@ import RxSwift
 
 class HomeViewReactor: Reactor {
     enum Action {
-      
+        case addPlayer
+        case removePlayer
     }
 
     enum Mutation {
-      
+        case increasePlayerCount
+        case decreasePlayerCount
     }
 
     struct State {
-      
+        var playerCount: Int
     }
     
-    let initialState = State()
+    let initialState: State
+    
+    init() {
+        self.initialState = State(
+            playerCount: 4
+        )
+    }
+    
+    func mutate(action: HomeViewReactor.Action) -> Observable<HomeViewReactor.Mutation> {
+        switch action {
+        case .addPlayer:
+            return Observable.just(Mutation.increasePlayerCount)
+        case .removePlayer:
+            return Observable.just(Mutation.decreasePlayerCount)
+        }
+    }
 
+    func reduce(state: HomeViewReactor.State, mutation: HomeViewReactor.Mutation) -> HomeViewReactor.State {
+        var newState = state
+        
+        switch mutation {
+        case .increasePlayerCount:
+            newState.playerCount += 1
+        case .decreasePlayerCount:
+            newState.playerCount -= 1
+        }
+
+        return newState
+    }
 }
