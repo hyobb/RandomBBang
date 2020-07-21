879f06e9f42907bbb3cb15fd1ed5ea3c7ff4dfc8
Ryu Hyo Kwang <hkryu0719@gmail.com>
2020-07-21 21:16:46 +0900

    feat: add shuffle logic

--- RandomBBang/ResultViewController.swift
+++ RandomBBang/ResultViewController.swift
@@ -22,6 +22,55 @@ class ResultViewController: UIViewController, View {
         
         print("ResultViewController Did Load")
         dump(reactor?.currentState.game)
+        
+        let game = reactor?.currentState.game
+        
+        let playerCount = game?.players.filter { !$0.isHidden }.count
+        let cost = game?.cost
+        let costDividedBy10 = (cost ?? 0) / 10
+        var costs: [Int] = []
+        
+        var ten = 10
+        
+        
+        guard let count = playerCount else { return }
+        
+        for i in 0..<count {
+            if ten > 0 {
+                if i == count - 1 {
+                    costs.append(ten * costDividedBy10)
+                } else {
+                    let num = Int.random(in: 0...ten)
+                    ten = ten - num
+                    costs.append(num * costDividedBy10)
+                }
+            } else {
+                costs.append(0)
+            }
+            
+            
+            print(i)
+        }
+        
+        
+        
+        let sum = costs.reduce(0, +)
+        
+        if sum != cost {
+            let index = costs.lastIndex(of: 0)
+            costs.remove(at: index!)
+            costs.append(cost! - sum)
+        }
+        
+        costs.shuffle()
+        print(costs)
+        print(costs.reduce(0, +) == cost)
+        
+        game?.players.filter { !$0.isHidden }.forEach { player in
+            player.cost = costs.popLast()!
+        }
+        
+        dump(game?.players)
     }
     
     func bind(reactor: ResultViewReactor) {
