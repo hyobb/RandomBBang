d492ec08e69a3c2ad1da245aa05876625d762921
Ryu Hyo Kwang <hkryu0719@gmail.com>
2020-09-11 09:26:58 +0900

    feat: add unit test

---
+++ RandomBBangTests/RandomBBangTests.swift
@@ -0,0 +1,53 @@
+//
+//  RandomBBangTests.swift
+//  RandomBBangTests
+//
+//  Created by 류효광 on 2020/09/10.
+//  Copyright © 2020 StudioX. All rights reserved.
+//
+
+import XCTest
+@testable import RandomBBang
+
+class RandomBBangTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testClassicStrategePlay() {
+        // given
+        let playerCount = 5
+        let cost = 100_000
+        let game = Game(playStrategy: ClassicStrategy())
+        game.playerCount = playerCount
+        game.cost = cost
+        
+        // when
+        game.play()
+        
+        // then
+        XCTAssertLessThan(game.players.filter({ (p: Player) -> Bool in
+            return !p.isHidden
+            }).count, playerCount)
+        XCTAssertEqual(cost, game.players.reduce(0, { (sum: Int, p: Player) -> Int in
+            return sum + p.cost
+        }))
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
